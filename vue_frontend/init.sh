7a7936993aa6345455ed86c241ffa80754ea6479
f5383 <mauricio.casagrande@fho.edu.br>
2025-12-10 09:20:30 -0300

    fix: build project files vue

--- vue_frontend/init.sh
+++ vue_frontend/init.sh
@@ -1,23 +1,21 @@
 #!/bin/sh
 set -e
 
-TARGET_DIR="/vue-app"
+echo "Projeto Vue em: $(pwd)"
 
-if [ ! -f "package.json" ]; then
-  echo "Criando projeto Vue em $TARGET_DIR..."
+if [ ! -f package.json ]; then
+  echo "Criando projeto Vue..."
 
   npm create vite@latest . -- --template vue
 
   echo "Instalando dependências..."
   npm install
 
-  echo "Ajustando permissões para o usuário $USER_ID:$GROUP_ID..."
-
-  chown -R $USER_ID:$GROUP_ID $TARGET_DIR
+  echo "Ajustando permissões..."
+  chown -R ${USER_ID}:${GROUP_ID} .
 else
   echo "Projeto Vue já existe"
 fi
 
-echo "Iniciando servidor de desenvolvimento..."
-
+echo "▶Iniciando servidor Vite..."
 npm run dev -- --host
