5b65ba84aea44d8a1b0c33b6750a6b8b2007ee4a
f5383 <mauricio.casagrande@fho.edu.br>
2025-12-10 09:54:11 -0300

    fix: build project files laravue

--- laravue/init.sh
+++ laravue/init.sh
@@ -1,35 +1,31 @@
 #!/bin/sh
 set -e
 
-TARGET_DIR="/var/www/html"
+echo "Projeto em: $(pwd)"
 
-if [ ! -f "$TARGET_DIR/artisan" ]; then
-  echo "Criando Laravue Monolito..."
+if [ ! -f artisan ]; then
+  echo "🚀 Criando Laravue Monolito"
 
   composer create-project laravel/laravel .
 
   cp .env.example .env
 
   touch database/database.sqlite
-  sed -i 's/DB_CONNECTION=mysql/DB_CONNECTION=sqlite/' .env
-  sed -i 's/DB_DATABASE=laravel/DB_DATABASE=\/var\/www\/html\/database\/database.sqlite/' .env
 
-  sed -i '/DB_HOST/d' .env
-  sed -i '/DB_PORT/d' .env
-  sed -i '/DB_USERNAME/d' .env
-  sed -i '/DB_PASSWORD/d' .env
+  sed -i 's/DB_CONNECTION=.*/DB_CONNECTION=sqlite/' .env
+  sed -i 's|DB_DATABASE=.*|DB_DATABASE=/var/www/html/database/database.sqlite|' .env
+
+  sed -i '/DB_HOST/d;/DB_PORT/d;/DB_USERNAME/d;/DB_PASSWORD/d' .env
 
   php artisan key:generate
   php artisan migrate --graceful
 
-  echo "Instalando Vue e dependências..."
+  echo "Instalando dependências frontend"
   npm install
   npm install vue @vitejs/plugin-vue
 
-  npm install -D @vitejs/plugin-vue
-
-  echo "Ajustando permissões para o usuário $USER_ID:$GROUP_ID..."
-  chown -R $USER_ID:$GROUP_ID $TARGET_DIR
+  echo "Ajustando permissões"
+  chown -R ${USER_ID}:${GROUP_ID} .
   chmod -R 775 storage bootstrap/cache
 else
   echo "Laravue já existe"
@@ -37,5 +33,4 @@ fi
 
 php artisan serve --host=0.0.0.0 --port=8000 &
 
-echo "Iniciando Vite..."
 npm run dev -- --host
